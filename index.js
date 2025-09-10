a8ca86b0ab75ac14fb16f587bc49586fdb547219
Mengi Mariam <mengimagheed7@gmail.com>
2025-09-11 01:19:54 +0300

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const logoName = document.querySelector(".logo-name")
 const header = document.querySelector("#header")
 const logo = document.querySelector(".logo")
 const hero = document.querySelector(" .hero")
+const heroSection = document.querySelector('.hero-section')
 menu.addEventListener("click", function(){
   menu.classList.toggle("show")
   mainNav.classList.toggle("show");
@@ -11,5 +12,6 @@ menu.addEventListener("click", function(){
   header.classList.toggle("show")
   logo.classList.toggle("show")
   hero.classList.toggle("show")
+  heroSection.classList.toggle("show")
 
 });
