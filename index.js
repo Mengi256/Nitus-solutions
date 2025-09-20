bc30868656ef5a6a36068e4f28fbada2a9e1e4c5
MengiCodes <mengimagheed7@gmail.com>
2025-09-20 18:30:46 +0300

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const logoName = document.querySelector(".logo-name")
 const header = document.querySelector("#header")
 const logo = document.querySelector(".logo")
 const hero = document.querySelector(" .hero")
+
 const heroSection = document.querySelector('.hero-section')
 menu.addEventListener("click", function(){
   menu.classList.toggle("show")
@@ -13,5 +14,14 @@ menu.addEventListener("click", function(){
   logo.classList.toggle("show")
   hero.classList.toggle("show")
   heroSection.classList.toggle("show")
+
+});
+
+document.addEventListener("DOMContentLoaded",function(){
+  const service_link = document.querySelector(" .service-link")
+ const service_container = document.querySelector(".service-container")
+ service_link.addEventListener("click",function(e){
+  e.preventDefault()
+ service_container.style.display = "block"})
 
 });
---
+++ index.py
@@ -0,0 +1,10 @@
+
+
+while True:
+  number = int(input("Enter number: "))
+  if number % 2 == 1:
+  
+     print("Odd numbers not supported")
+  else:
+     print("Number valid")
+     break
